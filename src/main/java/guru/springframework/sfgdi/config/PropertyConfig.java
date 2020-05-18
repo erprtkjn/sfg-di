75e7536c4fc546ab89be58878539d3b8a23f1512
Prateek Jain <jain.prateek904@gmail.com>
2020-05-18 17:16:54 +0200

    Usage of application.properties

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -2,25 +2,12 @@ package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import guru.springframework.sfgdi.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${prtk.username}")
     String user;
@@ -41,16 +28,16 @@ public class PropertyConfig {
     String jmsUrl;
 
     @Bean
-    public FakeDataSource fakeDataSource(){
+    public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
     }
 
     @Bean
-    public FakeJmsBroker fakeJmsBroker(){
+    public FakeJmsBroker fakeJmsBroker() {
         FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
         fakeJmsBroker.setUser(jmsUser);
         fakeJmsBroker.setPassword(jmsPassword);
@@ -58,9 +45,4 @@ public class PropertyConfig {
         return fakeJmsBroker;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
 }
